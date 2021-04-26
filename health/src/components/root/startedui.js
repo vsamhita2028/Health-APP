3f167de83aabf0ef2db321e569908f458add6aa5
Samhita Vetcha <52694492+vsamhita2028@users.noreply.github.com>
2021-04-26 22:19:29 +0530

    Navbar-commit

---
+++ health/src/components/root/startedui.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles((theme) => ({
+    title: {
+      flexGrow: 1,
+    },
+    pd :{
+        paddingRight : "1.5em"
+    }
+  }));
+  export  {useStyles}
